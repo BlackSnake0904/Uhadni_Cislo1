bbcf370a4199d0cb19eac3998554ff47184bc98e
BlackSnake0904 <117591705+BlackSnake0904@users.noreply.github.com>
2024-09-04 15:06:22 +0200

    Add project files.

---
+++ Uhadni cislo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Uhadni_Cislo;
+
+internal class Program
+{
+        
+    private static void Main(string[] args)
+    {
+       bool hraPokracuje = true;
+
+        Random cislo = new Random();
+        var nahodneCislo = cislo.Next(1,51);
+
+        Console.WriteLine("Witaj v hre uhadni cislo. Tvojou ulohou je uhadnut cislo na ktore PC mysli");
+        while (hraPokracuje)
+        {
+
+            Console.WriteLine("zadaj cislo medzi 1 - 50");
+            var input = Convert.ToInt32(Console.ReadLine());
+            if (input > 0 && input < 50)
+            {
+                if (nahodneCislo == input)
+                {
+                    Console.WriteLine("Zadal si spravne cislo");
+                    hraPokracuje = false;
+
+                }
+                else if (input < nahodneCislo) {
+                    Console.WriteLine("Zadal si nizke cislo oproti hladanemu \n ________________________________");
+                    
+
+                } else 
+                {
+                    Console.WriteLine("Zadal si vysoke cislo cislo oproti hladanemu \n ________________________________");
+                }
+                                
+            }
+            else
+            {
+                Console.WriteLine("Zadal si zle cislo. Gratulujem");
+            }
+        }
+
+        Console.WriteLine("Koniec hry");
+    }
+
+    
+}
